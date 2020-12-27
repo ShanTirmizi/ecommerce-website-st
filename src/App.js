bbad8166592edf781679d5967637f19783f2afdc
Shan Tirmizi <tirmizishahnawaz@gmail.com>
2020-12-27 14:49:32 +0000

    Revert "Merge pull request #2 from ShanTirmizi/product-page"
    This reverts commit e30f50c6a0ad793bc61bc8b6692e1acae82c710c, reversing
    changes made to ad04ac47f02b78212debbcf6d49056e7e32ba06a.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
+import Product from './components/Product';
+import data from './data';
 
 function App() {
   return (
-    <BrowserRouter>
     <div className="grid-container">
       <header className="row">
         <div>
@@ -19,13 +17,16 @@ function App() {
         </div>
       </header>
       <main>
-        <Route path='/' component={HomeScreen} exact></Route>
-        <Route path='/product/:id' component={ProductScreen} exact></Route>
-
+        <div>
+          <div className="row center">
+            {data.products.map((product) => (
+              <Product key={product.id} product={product}/>
+            ))}
+          </div>
+        </div>
       </main>
       <footer className="row center">All right reserved</footer>
     </div>
-    </BrowserRouter>
   );
 }
 
